b2b7e76eb9572ecc8be0483e91da002cffec09cf
tslight <tobyslight@gmail.com>
2018-08-09 12:05:41 +0100

    lazygitpush

--- cfortune/__init__.py
+++ cfortune/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2018, Toby Slight. All rights reserved.
+# ISC License (ISCL) - see LICENSE file for details.
+
 name = "cfortune"
 from .cfortune import cfortune
 from .cfortune import color
